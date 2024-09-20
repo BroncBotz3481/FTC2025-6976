c4aac977bd74a23f75d06c6f6dc61bec08524a4a
Void975 <150174899+Void975@users.noreply.github.com>
2024-09-20 12:35:40 -0500

    Added move left to Red Auto because I forgot

--- FTC-INTO-THE-DEEP-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team6976AutoRed2025.java
+++ FTC-INTO-THE-DEEP-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team6976AutoRed2025.java
@@ -17,6 +17,7 @@ public class Team6976AutoRed2025 extends LinearOpMode {
         moveForward(0.4, 1000); //moves forward
         moveRight(0.4, 0700);   //moves right
         moveForward(0.4, 1500); //moves forward
+        moveLeft(0.4, 0600); //moves left
 
 
 
