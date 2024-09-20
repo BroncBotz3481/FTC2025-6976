f0c98885aeb756f7ca19b5e26fd148241516b494
Void975 <150174899+Void975@users.noreply.github.com>
2024-09-19 19:40:35 -0500

    Added an Auto
    Don't use, it doesn't work

---
+++ FTC-INTO-THE-DEEP-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team6976AutoRed2025.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+@Autonomous (name="Team6976AutoRed2025", group="6976")
+public class Team6976AutoRed2025 extends LinearOpMode {
+
+    Team6976HM2025 robot = new Team6976HM2025();
+    ElapsedTime Time = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        robot.Map(hardwareMap);
+        waitForStart();
+
+        moveForward(0.4, 1650); //moves forward
+        
+
+    }
+
+    public void moveForward (double power, int time) {
+        robot.DriveRightFront.setPower(-power);
+        robot.DriveLeftFront.setPower(-power);
+        robot.DriveRightBack.setPower(-power);
+        robot.DriveLeftBack.setPower(-power);
+        sleep(time);
+        robot.DriveRightFront.setPower(0);
+        robot.DriveLeftFront.setPower(0);
+        robot.DriveRightBack.setPower(0);
+        robot.DriveLeftBack.setPower(0);
+    }
+    public void moveBackward (double power, int time){
+        robot.DriveRightFront.setPower(power);
+        robot.DriveLeftFront.setPower(power);
+        robot.DriveRightBack.setPower(power);
+        robot.DriveLeftBack.setPower(power);
+        sleep(time);
+        robot.DriveRightFront.setPower(0);
+        robot.DriveLeftFront.setPower(0);
+        robot.DriveRightBack.setPower(0);
+        robot.DriveLeftBack.setPower(0);
+    }
+    public void moveLeft (double power, int time){
+        robot.DriveRightFront.setPower(power);
+        robot.DriveLeftFront.setPower(-power);
+        robot.DriveRightBack.setPower(-power );
+        robot.DriveLeftBack.setPower(power);
+        sleep(time);
+        robot.DriveRightFront.setPower(0);
+        robot.DriveLeftFront.setPower(0);
+        robot.DriveRightBack.setPower(0);
+        robot.DriveLeftBack.setPower(0);
+    }
+
+}
